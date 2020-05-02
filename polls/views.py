c01a73e35c402472f9518dd9cc12087c272af748
Aayush Jain <Aayush Jain@DESKTOP-5T86111>
2020-05-02 12:36:10 +0530

    Index View Test Case

--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from .models import Question,Choice
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 class IndexView(generic.ListView):
     template_name='polls/index.html'
     context_object_name='latest_question'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 class DetailView(generic.DetailView):
     model=Question
     template_name='polls/detail.html'
