11ecbfa19bfe72ddc801005bd86a2128441de866
Aayush Jain <Aayush Jain@DESKTOP-5T86111>
2020-05-02 13:05:26 +0530

    Detail View Test Case

--- polls/tests.py
+++ polls/tests.py
@@ -35,6 +35,17 @@ class QuestionIndexViewTests(TestCase):
         response=self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['latest_question'],['<Question: Past question 2>','<Question: Past question 1>'])
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question=create_question(question_text='Future question',days=5)
+        url=reverse('polls:detail',args=(future_question.id,))
+        response=self.client.get(url)
+        self.assertEqual(response.status_code,404)
+    def test_past_question(self):
+        past_question=create_question(question_text='Past question',days=-5)
+        url=reverse('polls:detail',args=(past_question.id,))
+        response=self.client.get(url)
+        self.assertContains(response,past_question.question_text)
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
